491f2a230b95804a41ee500f18f8ac5ef1b29541
Lavender87 <shuiliansuixin@126.com>
2020-01-20 23:29:46 +0800

    Initial commit

---
+++ src/main/java/com/gupao/web/jenkinsdemo/controller/JenkinsController.java
@@ -0,0 +1,13 @@
+package com.gupao.web.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+
+    @GetMapping
+    public String findAll(){
+        return "findall";
+    }
+}
